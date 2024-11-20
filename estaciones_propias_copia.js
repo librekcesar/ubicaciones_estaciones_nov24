7f8088cb67a25496f439352c4057caf9fcfd10dc
librekcesar <140665209+librekcesar@users.noreply.github.com>
2024-11-20 13:06:44 -0600

    Add files via upload

---
+++ estaciones_propias_copia.js
@@ -0,0 +1,13 @@
+var puntosGeojson2 = {
+"type": "FeatureCollection",
+"name": "estaciones_propias",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Estacion": "MTY1", "Direccion": "PM6R+8G9 Monterrey, Nuevo León", "LATITUD": 25.7107875, "LONGITUD": -100.3086406 }, "geometry": { "type": "Point", "coordinates": [ -100.3086406, 25.7107875 ] } },
+{ "type": "Feature", "properties": { "Estacion": "VSA1", "Direccion": "Tecnoparque Bodega BI-6 CARRETERA ANACLETO CANABAL KM 0.400 RANCHERIA EMILIANO ZAPATA, MUNICIPIO DE CENTRO, TABASCO, C.P. 86280", "LATITUD": 17.9882745, "LONGITUD": -92.9738061 }, "geometry": { "type": "Point", "coordinates": [ -92.9738061, 17.9882745 ] } },
+{ "type": "Feature", "properties": { "Estacion": "MX4", "Direccion": "Hualquila 201 Bis, Santa Bárbara, Iztapalapa 09000 Ciudad de México", "LATITUD": 19.367613, "LONGITUD": -99.1023733 }, "geometry": { "type": "Point", "coordinates": [ -99.1023733, 19.367613 ] } },
+{ "type": "Feature", "properties": { "Estacion": "GDL1", "Direccion": "Av. de las Torres 5500, Camichines, 45580 San Pedro Tlaquepaque, Jal.", "LATITUD": 20.6241412, "LONGITUD": -103.2890745 }, "geometry": { "type": "Point", "coordinates": [ -103.2890745, 20.6241412 ] } },
+{ "type": "Feature", "properties": { "Estacion": "MX1", "Direccion": "Pical Pantaco. Priv. De Acalotenco 237 Bodega 46 Norte, San Sebastián, Azcapotzalco 02510 Ciudad de México", "LATITUD": 19.4806623, "LONGITUD": -99.1731841 }, "geometry": { "type": "Point", "coordinates": [ -99.1731841, 19.4806623 ] } },
+{ "type": "Feature", "properties": { "Estacion": "MX2", "Direccion": "Av. Mexiquense Lt. 6 No. 6 Nave 2B, Col. Ex. Hda. Los Portales, Municipio de Tultitlán, Estado de México, CP 54807", "LATITUD": 19.6422787, "LONGITUD": -99.1355893 }, "geometry": { "type": "Point", "coordinates": [ -99.1355893, 19.6422787 ] } }
+]
+}
